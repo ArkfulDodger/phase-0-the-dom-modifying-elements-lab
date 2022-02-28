d9e90e8a909670450f130ab8b35c21edb5b3d556
Noah Reece <noah.w.reece@gmail.com>
2022-02-28 11:50:29 -0500

    commit tree a68e11cf36d329c871f24d96c59e426244dd38b7

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here!
+// set main variable
+const main = document.getElementById('main');
+console.log(main.cloneNode(true));
+
+// set variable for parent of main to test child removal
+const mainParent = main.parentElement;
+console.log(mainParent.cloneNode(true));
+
+// test removal (one method at a time)
+// mainParent.removeChild(main); // test child removal
+main.remove(); // test direct element removal
+
+// verify element is removed, but still accessible in memory
+console.log(main.cloneNode(true));
+console.log(mainParent.cloneNode(true));
+
+//create h1 newHeader variable with id of victory
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+console.log(newHeader.cloneNode(true));
+
+// add text to newHeader
+newHeader.textContent = "Noah is the champion";
+console.log(newHeader.cloneNode(true));
